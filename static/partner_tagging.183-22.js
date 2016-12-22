7755e12a195748e9961a687620ff59d8eee34f34
huy-nguyen-colorado <huy@apps.directemployers.org>
2016-12-22 15:33:44 -0500

    PD-2548, dis-allow button submit if input is null

--- static/partner_tagging.183-22.js
+++ static/partner_tagging.183-22.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $("#addTags").on("click", function() {
+        if ($(this).hasClass("disabled")) {
+          return;
+        }
         setTimeout( function () {
-          if ($(this).hasClass("disabled")) {
-            return;
-          }
+
           var values = $("#p-tags").val();
           $.ajax({
             type: "GET",
