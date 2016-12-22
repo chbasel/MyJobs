e05ad2ff07a309632c675923bac4fe6f0ceb927f
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2016-12-22 12:00:22 -0500

    Editing the tabs

--- gulp/src/analytics/actions/tab-actions.js
+++ gulp/src/analytics/actions/tab-actions.js
@@ -1,19 +1,32 @@
+import {createAction} from 'redux-actions';
+
+export const switchActiveTab = createAction('SWITCH_ACTIVE_TAB');
+export const removeSelectedTab = createAction('REMOVE_SELECTED_TAB');
+
 /**
  * This action switches the current tab to a tab selected using the tabid
  */
-export function switchActiveTab(tabId) {
-  return {
-    type: 'SWITCH_ACTIVE_TAB',
-    payload: tabId,
+export function doSwitchActiveTab(tabId) {
+  return (dispatch) => {
+    dispatch(switchActiveTab(tabId));
   };
 }
 
 /**
  * This action removes a specific tab by clicking the x button on the tab using the tabid
  */
-export function removeSelectedTab(tabId) {
-  return {
-    type: 'REMOVE_SELECTED_TAB',
-    payload: tabId,
+export function doRemoveSelectedTab(tabId) {
+  return (dispatch, getState) => {
+    const selectedTab = getState().pageLoadData.navigation.filter(tab => tab.navId === tabId);
+    if (selectedTab[0].active) {
+      let maxNav = 0;
+      getState().pageLoadData.navigation.map(tab => {
+        if (!tab.active) {
+          maxNav = tab.navId > maxNav ? tab.navId : maxNav;
+        }
+      });
+      dispatch(switchActiveTab(maxNav));
+    }
+    dispatch(removeSelectedTab(tabId));
   };
 }
