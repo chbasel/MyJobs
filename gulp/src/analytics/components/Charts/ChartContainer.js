b524c0de569ad2e7a691130d7b7d2813f9e000b5
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2017-01-04 08:48:41 -0500

    Editing more of the tabs and charts for resizing

--- gulp/src/analytics/components/Charts/ChartContainer.js
+++ gulp/src/analytics/components/Charts/ChartContainer.js
@@ -24,10 +24,10 @@ class ChartContainer extends Component {
     let chartDisplay;
     switch (chartType) {
     case 'map:world':
-      chartDisplay = <WorldMap width={1920} height={600} scale={125} chartData={chartData} colorRange={ranges} />;
+      chartDisplay = <WorldMap width={1500} height={500} scale={100} chartData={chartData} colorRange={ranges} />;
       break;
     case 'map:nation':
-      chartDisplay = <USAMap width={1920} height={500} scale={1100} chartData={chartData} colorRange={ranges} />;
+      chartDisplay = <USAMap width={900} height={300} scale={600} chartData={chartData} colorRange={ranges} />;
       break;
     case 'map:state':
       chartDisplay = <SimpleBarChart width={600} height={400} chartData={chartData} />;
